ee4c7500c1a4690e21de81a2514f0f72d155cf23
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2024-10-14 00:11:45 +0530

    Added bookmark opening support (Partially). Needs Fixing

--- dashy.el
+++ dashy.el
@@ -21,35 +21,35 @@
 ;; Custom Variables ;;
 ;;;;;;;;;;;;;;;;;;;;;;
 
-;; (defcustom dashy-center-vertically t
-;;   "Center dashboard contents vertically"
-;;   :type 'boolean
-;;   :group 'dashy)
+(defcustom dashy-center-vertically t
+  "Center dashboard contents vertically"
+  :type 'boolean
+  :group 'dashy)
 
-;; (defcustom dashy-center-horizontally  t
-;;   "Center dashboard contents horizontally"
-;;   :type 'boolean
-;;   :group 'dashy)
+(defcustom dashy-center-horizontally  t
+  "Center dashboard contents horizontally"
+  :type 'boolean
+  :group 'dashy)
 
 ;; (defcustom dashy-center-image t
 ;;   "Center align the image"
 ;;   :type 'boolean
 ;;   :group 'dashy)
 
-(defcustom dashy-image nil
-  "Path to an image that will be displayed in the dashboard"
-  :type 'string
-  :group 'dashy)
+;; (defcustom dashy-image nil
+;;   "Path to an image that will be displayed in the dashboard"
+;;   :type 'string
+;;   :group 'dashy)
 
 (defcustom dashy-show-title t
   "Show title in the dashy dashboard"
   :type 'boolean
   :group 'dashy)
 
-(defcustom dashy-image-scale 8.0
-  "Scale of the dashy image displayed"
-  :type 'float
-  :group 'dashy)
+;; (defcustom dashy-image-scale 8.0
+;;   "Scale of the dashy image displayed"
+;;   :type 'float
+;;   :group 'dashy)
 
 (defcustom dashy-title "Dashy"
   "Title text displayed in the dashboard"
@@ -86,10 +86,10 @@
   :type 'int
   :group 'dashy)
 
-(defcustom dashy-show-image t
-  "Show image if t"
-  :type 'boolean
-  :group 'dashy)
+;; (defcustom dashy-show-image t
+;;   "Show image if t"
+;;   :type 'boolean
+;;   :group 'dashy)
 
 ;;;;;;;;;;;;;;;
 ;; Variables ;;
@@ -150,12 +150,18 @@
 
 (defun dashy--insert-header (text)
   "Returns a formatted text for header that takes in the text TEXT"
-  (insert (propertize (format "%s:\n\n" text) 'face 'dashy-header-face)))
+  (let* ((header (propertize (format "%s\n\n" text) 'face 'dashy-header-face)))
+    (if dashy-center-horizontally
+        (dashy--insert-center header)
+      (insert header))))
 
 (defun dashy--insert-title ()
   "Insert the dashy title into the dashy dashboard"
   (if dashy-show-title
-      (insert (propertize (dashy--get-title-text) 'face 'dashy-title-face))
+      (let* ((title (propertize (dashy--get-title-text) 'face 'dashy-title-face)))
+        (if dashy-center-horizontally
+            (dashy--insert-center title)
+          (insert title)))
     (if dashy-if-no-title-show-blank-lines (insert "\n\n"))))
 
 (defun dashy--insert-recent-files ()
@@ -167,8 +173,13 @@
             (let* ((files (if (equal dashy-num-recent-files -1)
                               recentf-list
                             (seq-take recentf-list dashy-num-recent-files))))
-              (dolist (file files)
-                (insert (format "- %s\n" (dashy--create-link file file)))))
+              (if dashy-center-horizontally
+                  (dolist (file files)
+                    (dashy--insert-center (dashy--create-link file file))
+                    (insert "\n"))
+                (dolist (file files)
+                  (insert (dashy--create-link file file))
+                  (insert "\n"))))
           (insert "No recent files\n"))
         (insert "\n"))))
 
@@ -177,28 +188,65 @@
   (if dashy-show-bookmarks
       (progn
         (dashy--insert-header "Bookmarks")
-        (let* ((bookmarks (if (equal dashy-num-bookmarks -1)
-                              (bookmark-all-names)
-                            (seq-take (bookmark-all-names) dashy-num-bookmarks))))
-          (if bookmarks
-              (dolist (bookmark bookmarks)
-                (insert (format "- %s\n" bookmark)))
-            (insert "No bookmarks found."))))))
+        (if dashy-center-horizontally
+            (dolist (bookmark bookmark-alist)
+              (let ((filename (cdr (assoc 'filename (cdr bookmark))))
+                    (bookmark-name (car bookmark)))
+                (dashy--insert-center (dashy--create-link bookmark-name filename))
+                (insert "\n")))
+          (dolist (bookmark bookmark-alist)
+            (let ((filename (cdr (assoc 'filename (cdr bookmark))))
+                  (bookmark-name (car bookmark)))
+              (insert (dashy--create-link bookmark-name filename))
+              (insert "\n")))))
+        (insert "No bookmarks found.")))
 
-(defun dashy--insert-image ()
-  "Insert image into dashboard"
-  (if dashy-show-image
-      (if (and (> (length dashy-image) 0) (file-exists-p dashy-image))
-          (let* ((img (create-image dashy-image nil nil :scale dashy-image-scale))
-                 (line-prefix
-                  `(space . (:align-to (- center ,(/ 100 2))))))
-            (insert-image img)
-            (insert "\n\n")))))
+(defun dashy--str-len (str)
+  "Calculate STR in pixel width."
+  (let ((width (frame-char-width))
+        (len (string-pixel-width str)))
+    (+ (/ len width)
+       (if (zerop (% len width)) 0 1))))  ; add one if exceeed
+
+(defun dashy--find-max-width (start end)
+  "Return the max width within the region START and END."
+  (save-excursion
+    (goto-char start)
+    (let ((width 0))
+      (while (< (point) end)
+        (let* ((line-str (buffer-substring (line-beginning-position) (line-end-position)))
+               (line-length (dashy--str-len line-str)))
+          (setq width (max width line-length)))
+        (forward-line 1))
+      width)))
+
+(defun dashy--center-text (start end)
+  "Center the text between START and END."
+  (let* ((width (dashy--find-max-width start end))
+         (prefix (propertize " " 'display `(space . (:align-to (- center ,(/ (float width) 2)))))))
+    (add-text-properties start end `(line-prefix ,prefix indent-prefix ,prefix))))
+
+(defun dashy--insert-center (&rest strings)
+  "Insert STRINGS in the center of the buffer."
+  (let ((start (point)))
+    (apply #'insert strings)
+    (dashy--center-text start (point))))
+
+;; (defun dashy--insert-image ()
+;;   "Insert image into dashboard"
+;;   (if dashy-show-image
+;;       (if (and (> (length dashy-image) 0) (file-exists-p dashy-image))
+;;           (let* ((img (create-image dashy-image nil nil :scale dashy-image-scale))
+;;                  (line-prefix
+;;                   `(space . (:align-to (- center ,(/ 100 2))))))
+;;             (insert-image img)
+;;             (insert "\n\n")))))
 
 (defun dashy-contents ()
   "Function that defines the content of the dashboard"
   ;; Image
-  (dashy--insert-image)
+
+  ;; (dashy--insert-image)
 
   ;; Title
   (dashy--insert-title)
